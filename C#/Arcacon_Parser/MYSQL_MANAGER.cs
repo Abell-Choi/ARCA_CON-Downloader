d4b8115163c81dacda032b706117c59c42385ac5
Abellelus-7074 <trooperalex92@gmail.com>
2023-04-10 13:38:44 +0900

    파일정리

---
+++ C#/Arcacon_Parser/MYSQL_MANAGER.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace Arcacon_Parser {
+	public class MYSQL_MANAGER {
+        string _DB_HOST;
+        int _DB_PORT = 3306;
+        string _DB_NAME;
+        string _ID;
+        string _PW;
+
+        public MYSQL_MANAGER ( string HOST, int PORT, string DB, string ID, string PW ){
+            this._DB_HOST = HOST;
+            this._DB_PORT = PORT;
+            this._DB_NAME = DB;
+            this._ID = ID;
+            this._PW = PW;
+		}
+	}
+}
---
+++ C#/Arcacon_Parser/Structor.cs
@@ -0,0 +1,53 @@
+﻿using System;
+namespace Arcacon_Parser
+{
+    /// <summary>  </summary>
+    public class Arca_Content_Jar {
+        string content_title { get; }                 // 아카콘 이름 (DB)
+        string post_url { get; }            // 아카콘 주소 (DB)
+        string upload_user { get; }                  // 업로드 유저 (DB)
+        int sell_count { get; }
+        public List<string> tags { get; }
+        DateTime? upload_time { get; }                          // 업로드 일시 (DB)
+        DateTime update_time { get; }                              // 업데이트 일시 (DB)
+
+        public Arca_Content_Jar(
+            string title, string post_url, string upload_user, int sell_count, List<string> tags, DateTime? upload_time
+        ) {
+            this.content_title = title;
+            this.post_url = post_url;
+            this.upload_user = upload_user;
+            this.sell_count = sell_count;
+            this.upload_time = upload_time;
+            this.update_time = DateTime.Now;
+        }
+    }
+
+    // FOR DB 
+    public class Arca_Content {
+
+        public int content_title { get; }
+        public string content_post_url { get; }
+        public int content_id { get; }
+        public string content_url { get; }
+        public bool isVideo { get; }
+
+        public Arca_Content(
+            int content_post_id, string content_post_url, int content_id, string content_url, bool isVideo
+            ) {
+            this.content_title = content_post_id;
+            this.content_post_url = content_post_url;
+            this.content_id = content_id;
+            this.content_url = content_url;
+            this.isVideo = isVideo;
+        }
+
+        public override string ToString()
+        {
+            new Dictionary<string, dynamic>();
+
+                return "";
+        }
+    }
+}
+
